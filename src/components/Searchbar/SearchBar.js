2880e8fba58bd2bceec31a2628796c966baff3f0
Aditya Kumar Panda <122735922+Adityapanda51@users.noreply.github.com>
2024-02-28 20:28:03 +0530

    Add files via upload

--- src/components/Searchbar/SearchBar.js
+++ src/components/Searchbar/SearchBar.js
@@ -4,9 +4,9 @@ import searchIcon from "../../Assets/Search icon.png";
 
 const SearchBar = () => {
   return (
-      <div class="searchBar">
+      <div className="searchBar">
       <input type="search" placeholder="Search an album of your choice" />
-      <div class="searchIconCont">
+      <div className="searchIconCont">
         <img src={searchIcon} class="searchImage" alt="search Icon" />
       </div>
       </div>
