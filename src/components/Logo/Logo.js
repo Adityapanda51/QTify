fef17c63ff1100f69e5429363e4848f389494e92
Aditya Kumar Panda <122735922+Adityapanda51@users.noreply.github.com>
2024-02-28 23:54:46 +0530

    Add files via upload

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,7 +1,12 @@
 import LogoImage from "../../Assets/logo.png";
+import "./Logo.css";
 
 const Logo = () => {
-  return <img src={LogoImage} alt="Logo" />;
+  return (
+    <div>
+      <img src={LogoImage} alt="Logo" className='logo'/>
+    </div>
+  ) 
 };
 
 export default Logo;
