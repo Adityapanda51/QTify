25077d45b7374b735700eaca75eb56c28d40b8fd
Aditya Kumar Panda <122735922+Adityapanda51@users.noreply.github.com>
2024-02-29 00:28:27 +0530

    Add files via upload
    hero section

--- src/Layout.js
+++ src/Layout.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import "./Layout.css";
+import Hero from './components/Hero/Hero';
 
 const Layout = () => {
   return (
     <div className='layout'>
       <Navbar />
+      <Hero />
     </div>
   )
 }
